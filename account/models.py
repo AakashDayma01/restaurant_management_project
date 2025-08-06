5cf4c85124d6fbaab1d1305aa2a1af8b722a2daa
AakashDayma01 <daymaaakash334@gmail.com>
2025-08-06 15:18:36 +0530

    22023

--- account/models.py
+++ account/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    """
+    Extends the default Django User model to store additional user profile information,
+    including full name, email, and phone number.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=225)
-    phone_number = moodels.charfield(max_length=15)
+    phone_number = moodels.CharField(max_length=15)
 
     def __str__(self):
-        return self.name
+        return self.user.get_full_name() or self.name
