1ba64c137d9bb57eb167b790edb8f146b90f35c4
AakashDayma01 <daymaaakash334@gmail.com>
2025-08-20 15:50:08 +0530

    22027

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import response
 # Create your views here.
+
+
+class MenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {'name': 'Margherita Pizza', 'description':'Classic cheese and tomato pizza','price':299},
+            {'name': 'Paneer Butter Masala','description':'Paneer cooked in creamy tomato gravy','price': 349},
+            {'name':'Veg Biryani','description':'Aromatic rice with mixed vegetsbles','price':249},
+            {'name':'Chocolate Brownie','description':'Rich chocolate dessert','price':199},
+        ]
+
+        return Response(menu)
