43f9e29845112cda4dd5c57c99bbb4bf8fd717c7
AakashDayma01 <daymaaakash334@gmail.com>
2025-08-20 15:25:44 +0530

    22025

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from menu.models import Menu
 # Create your models here.
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING','Pending'),
+        ('PROCESSING','Processing'),
+        ('COMPLETED','Completed'),
+        ('CANCELLED','Cancelled'),
+    ]
+
+    Customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    total_amount= models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default="PENDING")
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Order # {self.id} by {self.customer.username} - {self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    menu_item = models.ForeignKey(Menu, on_delete = models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name}"
+
+    def get_subtotal(self):
+        return self.menu_item.price * self.quantity
+
